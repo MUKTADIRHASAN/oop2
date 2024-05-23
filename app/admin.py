0fea055739c638480fbafe6be0e321ba73668fb6
muktadir <abirkbhs2020@gmail.com>
2024-05-23 12:09:43 +0600

    first commit

---
+++ app/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import *
+
+
+# Register your models here.
+
+class TagTublerInLine(admin.TabularInline):
+    model = Tag
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [TagTublerInLine]
+    list_display = ['tittle', 'author', 'date', 'status', 'section', 'Main_post']
+    list_editable = ['status', 'section', 'Main_post']
+    search_fields = ['tittle', 'author', 'section']
+
+admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag)
